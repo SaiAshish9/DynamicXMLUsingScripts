458336666cddc2f8c022dcdf3e282a89b1335058
saiashish9 <saiashish3760@gmail.com>
2022-04-11 21:38:06 +0530

    completed

--- scripts/xcui.py
+++ scripts/xcui.py
@@ -1,8 +1,14 @@
-# python3 xcui.py "SampleXCUITests" "Sample iOS.app" "com.bsstack.SampleXCUITests" "com.bsstack.SampleXCUITests"
+# python3 xcui.py "SampleXCUITests" "Sample iOS.app" "com.bsstack.SampleXCUITests" "com.browserstack.Sample-iOS"
 
 import json
+import subprocess
 from sys import argv
- 
+import os
+
+subprocess.run(['plutil', '-convert', 'json', 'xcui.xctestrun', '-o', 'xcui.json'],
+               stdout=subprocess.PIPE,
+               universal_newlines=True)
+
 f = open('xcui.json')
 tests_target = argv[1]
 testSuiteApp = argv[2]
@@ -15,23 +21,32 @@ data[tests_target] = data['SampleXCUITests']
 if 'SampleXCUITests' not in data:
     del data["SampleXCUITests"]
 data[tests_target][
-  "TestHostBundleIdentifier"
+    "TestHostBundleIdentifier"
 ] = f'{testSuiteBundleId}.xctrunner'
 data[tests_target][
-  "UITargetAppPath"
+    "UITargetAppPath"
 ] = f'__TESTROOT__/Debug-iphonesimulator/{testSuiteApp}'
 data[tests_target]["ProductModuleName"] = tests_target
 data[tests_target][
-  "TestHostPath"
+    "TestHostPath"
 ] = f'__TESTROOT__/Debug-iphonesimulator/{tests_target}-Runner.app'
-data[tests_target]["BundleIdentifiersForCrashReportEmphasis"][0] = appBundleId
-data[tests_target]["BundleIdentifiersForCrashReportEmphasis"][1] = testSuiteBundleId
+data[tests_target]["BundleIdentifiersForCrashReportEmphasis"][0] = testSuiteBundleId
+data[tests_target]["BundleIdentifiersForCrashReportEmphasis"][1] = appBundleId
 data[tests_target]["TestBundlePath"] = f'__TESTHOST__/PlugIns/{tests_target}.xctest'
-data[tests_target]["DependentProductPaths"][0] = f'__TESTROOT__/Debug-iphonesimulator/{testSuiteApp}'
-data[tests_target]["DependentProductPaths"][1] = f'__TESTROOT__/Debug-iphonesimulator/{tests_target}-Runner.app/PlugIns/{tests_target}.xctest'
+data[tests_target]["DependentProductPaths"][
+    0] = f'__TESTROOT__/Debug-iphonesimulator/{testSuiteApp}'
+data[tests_target]["DependentProductPaths"][
+    1] = f'__TESTROOT__/Debug-iphonesimulator/{tests_target}-Runner.app/PlugIns/{tests_target}.xctest'
 
-json_object = json.dumps(data, indent = 4)
-with open("xcui-1.json", "w") as outfile:
+json_object = json.dumps(data, indent=4)
+with open("xcui.json", "w") as outfile:
     outfile.write(json_object)
 
 f.close()
+
+subprocess.run(['plutil', '-convert', 'xml1', 'xcui.json', '-o', 'xcui.xctestrun'],
+               stdout=subprocess.PIPE,
+               universal_newlines=True)
+
+if os.path.exists("xcui.json"):
+    os.remove("xcui.json")
